9294a7d7d3ec3dc030478b0ca941d0b63046425c
donghoonchoi76 <lynus76@gmail.com>
2023-02-10 07:00:53 -0500

    Vacations interim

--- Vacations.cs
+++ Vacations.cs
@@ -1,7 +1,7 @@
 
 public class Vacation
-{
-    public DateTime Start { get; set; } = DateTime.Now.Date;
-    public DateTime End { get; set; }
-    public string Description { get; set; } = string.Empty;
-}
+    {
+        public DateTime Start { get; set; } = DateTime.Now.Date;
+        public DateTime End { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
